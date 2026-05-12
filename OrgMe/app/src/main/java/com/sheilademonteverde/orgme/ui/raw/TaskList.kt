dd395124d3593598da8bb31f49ebf3cebfc05b58
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-12 14:01:19 -0400

    Update TaskList.kt

--- OrgMe/app/src/main/java/com/sheilademonteverde/orgme/ui/raw/TaskList.kt
+++ OrgMe/app/src/main/java/com/sheilademonteverde/orgme/ui/raw/TaskList.kt
@@ -1,9 +1,28 @@
 /*
-Project Name: OrgMe
-Author: Sheila Demonteverde
-Module Name: TaskList
-Purpose: Object that contains a sublist of tasks items
-Date: 05/28/2020
+=====================================================
+ PROJECT: Android To Do List App (OrgMe)
+ MODULE: ui/raw/TaskList
+ AUTHOR: Sheila Demonteverde
+ PURPOSE: Object that contains a sublist of task items
+ DATE: 05/28/2020
+ Updated Dev Notes: 05/12/2026
+
+ DESCRIPTION:
+ This class represents a task list object
+ containing:
+ - a task list name
+ - a collection of task items
+
+ FEATURES:
+ - Parcelable implementation
+ - Stores task list name
+ - Stores multiple task items
+ - Supports Android object passing between activities
+
+ NOTES:
+ - Implements Parcelable for efficient data transfer
+ - Used in earlier raw-data implementation
+=====================================================
 */
 
 package com.sheilademonteverde.orgme.ui.raw
@@ -11,28 +30,32 @@ package com.sheilademonteverde.orgme.ui.raw
 import android.os.Parcel
 import android.os.Parcelable
 
-//The TaskList class takes a name then an array of tasks (strings)
+/*-- TASK LIST CLASS --*/
 class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList()) : Parcelable {
 
-    //
+    /*-- PARCEL CONSTRUCTOR --*/
     constructor(parcel: Parcel) : this(
         parcel.readString()!!,
         parcel.createStringArrayList()!!
-    )
+    ) // Reconstruct object from Parcel data
 
-    //
-    companion object CREATOR: Parcelable.Creator<TaskList> {
+    /*-- PARCELABLE CREATOR --*/
+    companion object CREATOR: Parcelable.Creator<TaskList>  
+    {
+        // Create object from Parcel
         override fun createFromParcel(source: Parcel): TaskList =
             TaskList(source)
 
-        override fun newArray(size: Int): Array<TaskList?> = arrayOfNulls(size)
+        override fun newArray(size: Int): Array<TaskList?> = arrayOfNulls(size) // Create Parcelable array
     }
 
-    //
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(name)
-        dest.writeStringList(tasks)
+    /*-- WRITE OBJECT TO PARCEL --*/
+    override fun writeToParcel(dest: Parcel, flags: Int) 
+    {
+        dest.writeString(name) // Write task list name
+        dest.writeStringList(tasks) // Write task items
     }
-    //
+    
+    /*-- DESCRIBE PARCEL CONTENTS --*/
     override fun describeContents() = 0
 }
